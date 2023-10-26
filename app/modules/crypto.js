82fc923a37638ffee86074d932cd07ecea7e1ab3
Oscar Henrichs <oscar.henrichs.work@outlook.com>
2023-10-26 08:18:21 -0300

    decrypt

--- app/modules/crypto.js
+++ app/modules/crypto.js
@@ -7,11 +7,11 @@ const encryptionIV = crypto.createHash("sha512").update(process.env.ENCRYPT_SECR
 
 function encryptData(data) {
 	const cipher = crypto.createCipheriv(ecnryption_method, key, encryptionIV);
-	return Buffer.from(cipher.update(data, "utf8", "hex") + cipher.final("hex")).toString("base64"); // Encrypts data and converts to hex and base64
+	return cipher.update(data, "utf8", "hex") + cipher.final("hex") // Encrypts data and converts to hex and base64
 }
 
 function decryptData(encryptedData) {
-	const buff = Buffer.from(encryptedData, "base64");
+	const buff = encryptedData
 	const decipher = crypto.createDecipheriv(ecnryption_method, key, encryptionIV);
 	return decipher.update(buff.toString("utf8"), "hex", "utf8") + decipher.final("utf8"); // Decrypts data and converts to utf8
 }
