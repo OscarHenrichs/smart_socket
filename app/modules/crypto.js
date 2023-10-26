f28837f4cbfab33a97a2ddf9895acc8ce42928f4
Oscar Henrichs <oscar.henrichs.work@outlook.com>
2023-10-26 08:21:54 -0300

    test

--- app/modules/crypto.js
+++ app/modules/crypto.js
@@ -1,19 +1,12 @@
-const crypto = require("crypto");
+var CryptoJS = require("crypto-js");
 require("dotenv-safe").config({ allowEmptyValues: true });
 
-const key = crypto.createHash("sha512").update(process.env.URL_SECRET).digest("hex").substring(0, 32);
-
-const encryptionIV = crypto.createHash("sha512").update(process.env.ENCRYPT_SECRET).digest("hex").substring(0, 16);
-
 function encryptData(data) {
-	const cipher = crypto.createCipheriv(ecnryption_method, key, encryptionIV);
-	return cipher.update(data, "utf8", "hex") + cipher.final("hex") // Encrypts data and converts to hex and base64
+	return CryptoJS.AES.encrypt("my message", process.env.ENCRYPT_SECRET).toString();
 }
 
 function decryptData(encryptedData) {
-	const buff = encryptedData
-	const decipher = crypto.createDecipheriv(ecnryption_method, key, encryptionIV);
-	return decipher.update(buff.toString("utf8"), "hex", "utf8") + decipher.final("utf8"); // Decrypts data and converts to utf8
+	return CryptoJS.AES.decrypt(ciphertext, process.env.ENCRYPT_SECRET).toString(); // Decrypts data and converts to utf8
 }
 
 module.exports = { encryptData, decryptData };
