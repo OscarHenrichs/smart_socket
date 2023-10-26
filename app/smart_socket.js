13c3e32333de509817de0d2dea23fee1dee42244
Oscar Henrichs <oscar.henrichs.work@outlook.com>
2023-10-26 05:35:05 -0300

    ttest

--- app/smart_socket.js
+++ app/smart_socket.js
@@ -52,6 +52,62 @@ require("uWebSockets.js")
 		},
 		open: (ws) => {
 			const userData = ws.getUserData();
+			ws.subscribe("test");
+		},
+		message: (ws, message, isBinary) => {
+			const userData = ws.getUserData();
+			ws.publish("test", message, isBinary);
+		},
+		drain: (ws) => {},
+		close: (ws, code, message) => {
+			ws.unsubscribe("test");
+		},
+	})
+	.ws("/task/:project_id/:task_id", {
+		idleTimeout: 120,
+		maxBackpressure: 1024,
+		maxPayloadLength: 512,
+		upgrade: async (res, req, context) => {
+			const database = require("./database/database.js")();
+			res.onAborted(() => {
+				res.aborted = true;
+			});
+			console.log("An Htts connection wants to become WebSocket, URL: " + req.getUrl() + "!");
+
+			try {
+				const key = req.getHeader("sec-websocket-key");
+				const protocol = req.getHeader("sec-websocket-protocol");
+				const extensions = req.getHeader("sec-websocket-extensions");
+				const projectId = req.getParameter(0);
+				const taskId = req.getParameter(1);
+				console.log(projectId, taskId);
+				const token = req.getHeader("authorization");
+
+				res.user = await decodeJWT(token, database);
+
+				if (res.aborted) {
+					return;
+				}
+
+				if (res.user.auth != undefined) {
+					return res
+						.cork(() => {
+							res.writeStatus("401").write(JSON.stringify({ error: "Unauthorized A" }));
+						})
+						.endWithoutBody();
+				}
+
+				return res.upgrade({ user_id: res.user, task_id: taskId, project_id: projectId }, key, protocol, extensions, context);
+			} catch {
+				return res
+					.cork(() => {
+						res.writeStatus("401").write(JSON.stringify({ error: "Unauthorized B" }));
+					})
+					.endWithoutBody();
+			}
+		},
+		open: (ws) => {
+			const userData = ws.getUserData();
 			ws.subscribe(`${constants.broadCastProject}/${userData.projectId}`);
 			ws.subscribe(`${constants.broadCastTask}/${userData.taskId}`);
 			ws.subscribe(`${constants.broadCastUser}/${userData.user_id}`);
